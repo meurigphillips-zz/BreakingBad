0a1dfbc582977c965c501893336a7d6e7e0e76e7
Meurig Phillips <meurigp@gmail.com>
2020-09-13 16:57:05 +0100

    Detail VC working

--- BreakingBadBluePrint/CharacterDetail/CharacterDetailViewController.swift
+++ BreakingBadBluePrint/CharacterDetail/CharacterDetailViewController.swift
@@ -7,13 +7,37 @@
 //
 
 import UIKit
+import SDWebImage
 
 class CharacterDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var seasonLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var character: Character?
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureView()
+    }
 
-        // Do any additional setup after loading the view.
+    private func configureView() {
+        guard let character = character else { return }
+        nameLabel.text = character.name
+        nicknameLabel.text = character.nickname
+        var occupationString = ""
+        for occupation in character.occupation {
+            occupationString += "\n‣ \(occupation)"
+        }
+        occupationLabel.text = "Occupation(s):\(occupationString)"
+        statusLabel.text = "Status: \(character.status)"
+        let seasons = character.appearance.map(String.init).joined(separator: ", ")
+        seasonLabel.text = "Appears in season(s): \(seasons)"
+        imageView.sd_setImage(with: URL(string: character.img), placeholderImage: UIImage(systemName: "questionmark.diamond"))
     }
     
 
--- BreakingBadBluePrint/CharactersList/CharactersListViewController.swift
+++ BreakingBadBluePrint/CharactersList/CharactersListViewController.swift
@@ -18,12 +18,21 @@ class CharacterListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UINib(nibName: "CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
-        tableView.delegate = self
-        tableView.dataSource = self
         
         viewModel.displayedCharacters.bind { [weak self] characters in
             self?.tableView.reloadData()
         }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard
+            segue.identifier == "ShowDetailSegue",
+            let indexPathRow = tableView.indexPathForSelectedRow?.row,
+            let detailViewController = segue.destination as? CharacterDetailViewController
+            else {
+                return
+        }
+        detailViewController.character = viewModel.displayedCharacters.value[indexPathRow]
     }
     
 }
@@ -35,8 +44,13 @@ extension CharacterListViewController: UITableViewDataSource, UITableViewDelegat
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? CharacterTableViewCell else { return UITableViewCell() }
+        cell.selectionStyle = .none
         cell.setup(character: viewModel.displayedCharacters.value[indexPath.row])
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "ShowDetailSegue", sender: nil)
     }
 }
 
